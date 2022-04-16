8ae1c39755b49a8a2ed53ed2a67e7914eb9e8ae5
SaceKylaPatrice <kylapatricesoberanosace@gmail.com>
2022-04-15 16:29:15 -1200

    song2 added

---
+++ song2.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Areuminct-Lovers Music Lyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Ginintuang Tanawin</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Ikaw Ang Musika</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Kahel Na Langit</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Ikaw Ang Pahinga Ko</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Marupok</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-2 text-center">
+                <h1 class="fw-bolder">Welcome to Areuminct-Lovers Music Lyrics</h1>
+                <p class="lead">“This is inspired with the wattpad stories  written by Areuminct. Music is your own experience, your thoughts, your wisdom. If you don’t live it, it won’t come out of your horn.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-2">
+                <div class="row gx-6 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Ikaw Ang Musika</h2>";
+                        echo "<p class='lead '>Marc ALfonso Ft. Gwy Saludes</p>";
+                        echo "<br/>";
+
+                        //1st stanza
+                        echo "<p> I:</p>";
+                        echo "<p> Pagmulat ng mata kasama na kita</p>";
+                        echo "<p> Sinasangga ang laban, sa’n man tayo magpunta</p>";
+                        echo "<p> Sunud-sunuran sa laruan hanggang sa eskwela</p>";
+                        echo "<p> Di na humiwalay, tayo na ba ang tinadhana?</p>";
+
+                        echo "<br/>";
+
+                        echo "<p> Nagkagusto man ako sa kaniya </p>";
+                        echo "<p> Ikaw pa rin ang nais kong makasama</p>";
+                        echo "<p> Walang iba</p>";
+                        
+
+                        echo "<br/>";
+
+
+
+                        //Pre Chorus
+                        echo "<br/>";
+                        echo "<p> Pre-Chorus:</p>";
+                        echo "<p> Bawat tingin,</p>";
+                        echo "<p> Tono sa tenga’y humihiling</p>";
+                        echo "<p> Pagkat sa bawat minuto,</p>";
+                        echo "<p> Gusto kang pagmasdan.</p>";
+                        
+                        echo "<br/>"; 
+
+                        echo "<p> Ewan ko ba,</p>";
+                        echo "<p> Ano ba ‘tong nadarama</p>";
+                        echo "<p> humahalo sa agos ng pusong takang-taka.</p>";                       
+                        
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus:</p>";
+                        echo "<p> Ikaw na ang laman ng musika</p>";
+                        echo "<p> Sa ilalim ng langit</p>";
+                        echo "<p> Bawat tinig sa gitara</p>";
+                        echo "<p> Sabay sa bagyo ng isip</p>";
+                       
+                        echo "<br/>";
+
+                        echo "<p> Paano nga ba sasabihin</p>";
+                        echo "<p> Kung lumalim ang damdamin</p>";
+                        echo "<p> Hahayaan ba o aking aaminin?</p>";
+                        echo "<p> Paano na</p>";
+                                              
+                        echo "<br/>";
+
+                        echo "<p> Oooh</p>";
+
+
+                        echo "<br/>";
+                                              
+
+
+                        //2nd stanza
+                        echo "<br/>";
+                        echo "<p> II:</p>";
+
+                        echo "<p> Pagtapos ng araw, ako lang at ikaw</p>";
+                        echo "<p> Ganda ng ulap, ang damdamin ko’y umaapaw</p>";
+                        echo "<p> Nakaupo, magkatabi’t kasama ang gitara</p>";
+                        echo "<p> Nakatingin sa ‘yo, ramdam ang pusong guminhawa~</p>";
+
+                        echo "<br/>";
+
+                        echo "<p> Kaba sa puso nararamdaman </p>";
+                        echo "<p> Ngayon na ba o kailan mo malalamang</p>";
+                        echo "<p> Mahal kita</p>";
+                        
+
+                        echo "<br/>";
+                       
+                        //Pre Chorus
+                        echo "<br/>";
+                        echo "<p> Pre-Chorus:</p>";
+                        echo "<p> Bawat tingin,</p>";
+                        echo "<p> Tono sa tenga’y humihiling</p>";
+                        echo "<p> Pagkat sa bawat minuto,</p>";
+                        echo "<p> Gusto kang pagmasdan.</p>";
+                        
+                        echo "<br/>"; 
+
+                        echo "<p> Ewan ko ba,</p>";
+                        echo "<p> Ano ba ‘tong nadarama</p>";
+                        echo "<p> humahalo sa agos ng pusong takang-taka.</p>";                       
+                        
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus:</p>";
+                        echo "<p> Ikaw na ang laman ng musika</p>";
+                        echo "<p> Sa ilalim ng langit</p>";
+                        echo "<p> Bawat tinig sa gitara</p>";
+                        echo "<p> Sabay sa bagyo ng isip</p>";
+                       
+                        echo "<br/>";
+
+                        echo "<p> Paano nga ba sasabihin</p>";
+                        echo "<p> Kung lumalim ang damdamin</p>";
+                        echo "<p> Hahayaan ba o aking aaminin?</p>";
+                        echo "<p> Paano na</p>";                       
+                        echo "<br/>";
+
+
+
+                        //Refrain
+                        echo "<br/>";
+                        echo "<p> Refrain:</p>";
+                        echo "<p> Hindi ko na kayang manatili pa</p>";
+                        echo "<p> Huwag mo sa ‘king sasabihing tama na</p>";
+                        echo "<p> Ayoko na. Sana hindi ka na nakilala.</p>";
+                        echo "<p> Naaalala mong nangako ka, ‘di ba?</p>";
+
+                        echo "<br/>";
+
+                       
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus:</p>";
+                        echo "<p> Ikaw pa rin ang laman ng musika…</p>";
+                        echo "<p> Bawat sigaw ng melodiya</p>";
+                        echo "<p> Sabay sa tibok ng puso</p>";
+ 
+                       
+                        echo "<br/>";
+
+                        echo "<p> Habang-buhay,</p>";
+                        echo "<p> Hawak-hawak ang iyong kamay</p>";
+                        echo "<p> Mananatili sa iyong tabi</p>";
+                    
+                       
+                        echo "<br/>";
+
+                        echo "<p> Pangako, di ka bibitawan</p>"; 
+                        echo "<p> Pangako, di ka bibitawan...</p>";   
+
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Kyla Patrice Soberano Sace </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
